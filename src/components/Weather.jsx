a0afb05974b35e7590be1f28bbb709a58561b540
Lizeth Gutierrez <lizethd.gutierrez11@gmail.com>
2022-08-07 20:11:38 -0500

    Despliege de ubicacion y conversion de temperatura

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,9 +1,21 @@
 import axios from 'axios'
 import { useState, useEffect } from "react"
 
-function Weather () {
-    const [apiResponse, setApiResponse] = useState({});
-    // const [coords, setCoords] = useState({ latitude: 0, longitude : 0});
+function fromKelvinToCelsius(kelvin) {
+    return Math.round(kelvin - 273, 2);
+}
+
+function fromKelvinToFahrenheit(kelvin) {
+    return Math.round(1.8 * (kelvin - 273) + 32);
+}
+
+function Weather() {
+    const [isCelsius, setIsCelsius] = useState(true);
+    const [weather, setWeather] = useState({
+        temp: 273,
+        city: "",
+        country: ""
+    });
     const currentLocation = {
         latitude: 0,
         longitude: 0,
@@ -11,35 +23,40 @@ function Weather () {
     useEffect(() => {
         const APIURL = "https://api.openweathermap.org/data/2.5/weather";
         const APIKEY = "00071cecea4543a4f30d61b7bb725655";
-        // ?lat=21.8590464&lon=-102.2891426&appid=00071cecea4543a4f30d61b7bb725655
-        
+
         navigator.geolocation.getCurrentPosition(location => {
             console.log(location)
-            // setCoords({
-            //     latitude: location.coords.latitude, 
-            //     longitude: location.coords.longitude,
-            // })
+
             currentLocation.latitude = location.coords.latitude;
             currentLocation.longitude = location.coords.longitude;
-            
+
             axios.get(APIURL, {
-                params:{
-                    lat: currentLocation.latitude, 
+                params: {
+                    lat: currentLocation.latitude,
                     lon: currentLocation.longitude,
                     appid: APIKEY
                 }
-            })
-                .then(response => {
-                    console.log(response.data);
-                    setApiResponse(response.data);
+            }).then(response => {
+                console.log(response.data);
+
+                setWeather({
+                    temp: response.data.main.temp,
+                    city: response.data.name,
+                    country: response.data.sys.country
                 });
+            });
         })
     }, [currentLocation.latitude, currentLocation.longitude])
-    
+
+    const setTemperaturePreference = function () {
+        setIsCelsius(!isCelsius)
+    }
+
     return (
         <div>
-            <p>Clima</p>
-            <p>{apiResponse.main?.temp} K</p>
+            <p>Clima en {weather.city}, {weather.country}</p>
+            <p>{isCelsius ? fromKelvinToCelsius(weather.temp) : fromKelvinToFahrenheit(weather.temp)} {isCelsius ? '°C' : '°F'}</p>
+            <button onClick={setTemperaturePreference}>{isCelsius ? '°F' : '°C'}</button>
         </div>
     )
 }
